18cfc43e0a75c10229e535b7907cf0f5456addd1
nuomi1 <nuomi1@qq.com>
2024-07-02 00:38:23 +0800

    :sparkles: Add test text.

--- TestDynamicType/Sources/Constants.swift
+++ TestDynamicType/Sources/Constants.swift
@@ -6,11 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+import UIKit
 
 enum Constants {
 
     enum Tab {
         static let swiftUI = TabContentViewConfiguration(tabTitle: "SwiftUI", tabIcon: "sun.max", navigationTitle: "SwiftUI Dynamic Type")
         static let uiKit = TabContentViewConfiguration(tabTitle: "UIKit", tabIcon: "moon", navigationTitle: "UIKit Dynamic Type")
+    }
+
+    enum Font {
+        static var swiftUI: [SwiftUI.Font] { [
+            // .extraLargeTitle,
+            // .extraLargeTitle2,
+            .largeTitle,
+            .title,
+            .title2,
+            .title3,
+            .headline,
+            .subheadline,
+            .body,
+            .callout,
+            .caption,
+            .caption2,
+            .footnote,
+        ] }
+
+        static var uiKit: [UIFont] { [
+            .extraLargeTitle,
+            .extraLargeTitle2,
+            .largeTitle,
+            .title1,
+            .title2,
+            .title3,
+            .headline,
+            .subheadline,
+            .body,
+            .callout,
+            .caption1,
+            .caption2,
+            .footnote,
+        ].map { UIFont.preferredFont(forTextStyle: $0) }}
     }
 }
--- TestDynamicType/Sources/TestDynamicTypeApp.swift
+++ TestDynamicType/Sources/TestDynamicTypeApp.swift
@@ -5,6 +5,7 @@
 //  Created by nuomi1 on 2024-06-26.
 //
 
+import Combine
 import Foundation
 import SwiftData
 import SwiftUI
@@ -12,18 +13,71 @@ import SwiftUI
 @main
 struct TestDynamicTypeApp: App {
 
+    @State
+    private var dynamicTypeSize: DynamicTypeSize?
+
     var body: some Scene {
         WindowGroup {
-            TabView {
-                TripTabContentView(configuration: Constants.Tab.swiftUI) { trip in
-                    SwiftUITripDetailView(trip: trip)
-                }
+            // tabView
 
-                TripTabContentView(configuration: Constants.Tab.uiKit) { trip in
-                    UIKitTripDetailView(trip: trip)
-                }
-            }
+            text
         }
         .modelContainer(for: [Trip.self])
+    }
+
+    @ViewBuilder
+    private var tabView: some View {
+        TabView {
+            TripTabContentView(configuration: Constants.Tab.swiftUI) { trip in
+                SwiftUITripDetailView(trip: trip)
+            }
+
+            TripTabContentView(configuration: Constants.Tab.uiKit) { trip in
+                UIKitTripDetailView(trip: trip)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private var text: some View {
+        let text = String(repeating: "Hello, World! ", count: 10)
+
+        VStack {
+            Text(text)
+                .font(.body)
+
+            UIKitLabel(text: text, font: .preferredFont(forTextStyle: .body))
+        }
+        .onAppear {
+            let sizeCategory = UITraitCollection.current.preferredContentSizeCategory
+            dynamicTypeSize = .init(sizeCategory)
+        }
+        .onChange(of: dynamicTypeSize) { _, _ in
+            printFont()
+        }
+        .onReceive(publisher) { notification in
+            let sizeCategory = notification.userInfo![UIContentSizeCategory.newValueUserInfoKey]! as! UIContentSizeCategory
+            dynamicTypeSize = .init(sizeCategory)
+        }
+    }
+
+    private var publisher: NotificationCenter.Publisher {
+        NotificationCenter.default.publisher(for: UIContentSizeCategory.didChangeNotification)
+    }
+
+    private func printFont() {
+        dump(dynamicTypeSize!)
+
+        print("SwiftUI")
+
+        for font in Constants.Font.swiftUI {
+            print(font)
+        }
+
+        print("UIKit")
+
+        for font in Constants.Font.uiKit {
+            print(font)
+        }
     }
 }
